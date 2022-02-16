0b736d6d7c8406f49736ab72e1caa0a266e512b1
Silvan Greverus <info@sgapps.de>
2022-02-16 07:17:58 +0100

    Chalk compatibility import

---
+++ src/chalk.ts
@@ -0,0 +1,31 @@
+/********************************************************************/
+/*                                                                  */
+/*  chalk style import                                              */
+/*                                                                  */
+/********************************************************************/
+
+import { ConsoleStyler, ConsoleStylerOptions } from './console-styler'
+
+export type ColorSupport = false | { hasBasic: boolean, has256: boolean, has16m: boolean }
+
+function level2ColorSupport(level: number): ColorSupport {
+
+    if (level<1) return false;
+
+    return { hasBasic: true, has256: level>=2, has16m: level>=3 };
+}
+
+export class Chalk extends ConsoleStyler {
+
+    constructor(opts: ConsoleStylerOptions) {
+        super(opts);
+    }
+}
+
+const chalk = new ConsoleStyler();
+export const chalkStderr = new ConsoleStyler({stderr: true});
+
+export const supportsColor = level2ColorSupport(chalk.level);
+export const supportsColorStderr = level2ColorSupport(chalkStderr.level);
+
+export default chalk;
